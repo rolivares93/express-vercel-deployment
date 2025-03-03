393a72b3e39da25ee2fbe703cf9d379dd89f8d80
Rodrigo Olivares <rolivares6793@gmail.com>
2025-03-03 16:36:23 -0300

    added port to feedback message

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ const app = express()
 
 const port = process.env.PORT || 4000
 
-app.get('/', (req, res) => res.send('Express server on Vercel'))
+app.get('/', (req, res) => res.send(`Express server on Vercel port ${port}`))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
