eebd850f61db65b9ed44de69c7b9412ca3d8fa61
Rodrigo Olivares <rolivares6793@gmail.com>
2025-03-03 16:41:03 -0300

    added http dependency

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import http from 'http'
 
 dotenv.config()
 
 const app = express()
+const httpServer = http.createServer(app)
 
 const port = process.env.APP_PORT || 4000
 
 app.get('/', (req, res) => res.send(`Express server on Vercel port ${port}`))
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+httpServer.listen(port, () => console.log(`Server started on port ${port}`))
