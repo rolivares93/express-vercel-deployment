6690b3c0f97849c014672c1e4d835295994ef622
Rodrigo Olivares <rolivares6793@gmail.com>
2025-03-03 16:29:29 -0300

    small change to / route response

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import express from 'express'
-const app = express();
+const app = express()
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
 
-app.get("/", (req, res) => {
-  res.send("Subscribe to Arpan Neupane's channel");
-});
+app.get('/', (req, res) => {
+  res.send('Express server on Vercel')
+})
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+  console.log(`Server started on port ${port}`)
+})
