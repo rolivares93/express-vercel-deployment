51d82e156ad33635687db531b3a8b3d7d683a0d9
Rodrigo Olivares <rolivares6793@gmail.com>
2025-03-03 15:27:52 -0300

    changed port from 8080 to 4000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("Subscribe to Arpan Neupane's channel");
