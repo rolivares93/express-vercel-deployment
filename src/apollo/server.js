16886ab7d10528f0893aec8d60490091a6b1cd93
Rodrigo Olivares <rolivares6793@gmail.com>
2025-03-04 11:33:50 -0300

    added apollo/server and its basic config

---
+++ src/apollo/server.js
@@ -0,0 +1,6 @@
+import { ApolloServer } from '@apollo/server'
+
+export const apolloServer = new ApolloServer({
+  typeDefs: ``,
+  resolvers: {},
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import http from 'http'
+import { apolloServer } from './apollo/server.js'
 
 dotenv.config()
 
@@ -9,7 +10,14 @@ const app = express()
 const httpServer = http.createServer(app)
 const port = process.env.APP_PORT || 4000
 
+await apolloServer.start()
+
 app.use(cors())
+
+app.use(
+  '/graphql',
+  express.json(),
+)
 
 app.get('/', (req, res) => res.send(`Express server on Vercel port ${port}`))
 
