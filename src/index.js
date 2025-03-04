0eba0ef04f710a31cc0a11eebc53a21404dd8395
Rodrigo Olivares <rolivares6793@gmail.com>
2025-03-04 11:26:56 -0300

    added cors default

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import express from 'express'
+import cors from 'cors'
 import dotenv from 'dotenv'
+import express from 'express'
 import http from 'http'
 
 dotenv.config()
 
 const app = express()
 const httpServer = http.createServer(app)
-
 const port = process.env.APP_PORT || 4000
+
+app.use(cors())
 
 app.get('/', (req, res) => res.send(`Express server on Vercel port ${port}`))
 
