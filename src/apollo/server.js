b0b4984740e84e016f175cc35fe22d8facc80f4e
Rodrigo Olivares <rolivares6793@gmail.com>
2025-03-04 11:52:05 -0300

    changed apollo package and updated its configs

--- src/apollo/server.js
+++ src/apollo/server.js
@@ -1,6 +1,9 @@
-import { ApolloServer } from '@apollo/server'
+import { ApolloServer } from 'apollo-server-express'
+import { resolvers } from './resolvers.js'
+import { schema } from './schema.js'
 
 export const apolloServer = new ApolloServer({
-  typeDefs: ``,
-  resolvers: {},
+  typeDefs: schema,
+  resolvers: resolvers,
+  introspection: true,
 })
--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import http from 'http'
-// import { apolloServer } from './apollo/server.js'
+import { apolloServer } from './apollo/server.js'
 
 dotenv.config()
 
-const app = express()
-const httpServer = http.createServer(app)
-const port = process.env.APP_PORT || 4000
+async function startServer() {
+  const app = express()
+  const httpServer = http.createServer(app)
+  const port = process.env.APP_PORT || 4000
 
-// await apolloServer.start()
+  await apolloServer.start()
+  apolloServer.applyMiddleware({ app })
 
-app.use(cors())
+  app.get('/', (req, res) => res.send(`Express server on Vercel port ${port}`))
 
-// app.use(
-//   '/graphql',
-//   express.json(),
-// )
+  httpServer.listen(port, () => console.log(`Server started on port ${port}`))
+}
 
-app.get('/', (req, res) => res.send(`Express server on Vercel port ${port}`))
-
-httpServer.listen(port, () => console.log(`Server started on port ${port}`))
+startServer()
